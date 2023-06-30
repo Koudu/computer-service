8e4cfb6db6ba78fe65e91614005e3adf2c8a3e9a
Koudu <gorb.andrey.08@gmail.com>
2023-06-30 12:31:31 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    scr/main.js 	deleted:    scr/style.css 	new file:   src/main.js 	new file:   src/style.css 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
@@ -18,16 +17,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "public/index.html",
       inject: "body",
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "./public/manifest.json", to: "" },
-        { from: "./src/sw.js", to: "" },
-        { from: "./public/avatar-96x96.png", to: "" },
-        { from: "./public/avatar-144x144.png", to: "" },
-        { from: "./public/avatar-256x256.png", to: "" },
-        { from: "./public/avatar-512x512.png", to: "" },
-      ],
     }),
   ],
   module: {
