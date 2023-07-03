c348997ace0497f8fd60a20c94a2b7ffdfae7f72
Koudu <gorb.andrey.08@gmail.com>
2023-07-03 11:48:47 +0300

    new file:   public/basket.html 	modified:   public/index.html 	modified:   public/menu.svg 	modified:   public/mouse.svg 	new file:   public/product.html 	modified:   src/main.js 	modified:   src/style.css

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+// const body = document.querySelector("body");
+// const menuPanel = document.getElementById("menu-panel");
+
+// body?.addEventListener("click", (e) => {
+//   const name = e.target.getAttribute("name");
+//   if (name === "menu-btn" && menuPanel) {
+//     console.log(name);
+//     if (menuPanel?.classList.contains("_inactive")) {
+//       menuPanel.classList.remove("_inactive");
+//       menuPanel.classList.add("_active");
+//       console.log("1 if");
+//       return;
+//     }
+//     menuPanel.classList.add("_inactive");
+//     menuPanel.classList.remove("_active");
+//     console.log("2 if");
+//   }
+// });
